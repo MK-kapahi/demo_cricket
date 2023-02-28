ea1c9f81112a7714b654a6c1d83b193a579edce4
MK-kapahi <Muskan@chicmic.co.in>
2023-02-28 18:38:40 +0530

    this commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PlayerComponent } from './player/player.component';
+import { HttpClientModule } from '@angular/common/http'
+import { PlayerService } from './Services/player.service';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PlayerComponent
+    PlayerComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [PlayerService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  constructor(public service :PlayerService){}
+}
--- src/app/player/player.component.ts
+++ src/app/player/player.component.ts
@@ -1,5 +1,6 @@
-import { Component } from "@angular/core";
-import { FormControl, FormGroup } from "@angular/forms";
+import { Component} from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
+import { AppModule } from "../app.module";
 
 @Component({
     selector : 'app-player',
@@ -9,12 +10,42 @@ import { FormControl, FormGroup } from "@angular/forms";
 
 export class PlayerComponent {
 
-    playerform = new FormGroup({
-        name : new FormControl(''),
-        age : new FormControl(''),
-        id :new FormControl(''),
-        type : new FormControl(''),
-        no_of_matches : new FormControl(''),
+    constructor(private add :AppModule){}
 
+    playerform = new FormGroup({
+        name :      new FormControl('',[Validators.required,Validators.minLength(4)]),
+        age : new FormControl(18,[Validators.required,Validators.min(18),Validators.max(50)]),
+        id :new FormControl('',Validators.required),
+        team : new FormControl('',Validators.required),
+        no_of_matches : new FormControl(0,Validators.required),
+        type : new FormControl('Allrounder',Validators.required,)
     })
+
+    submit()
+    {
+        this.add.service.addplayer(this.playerform.value).subscribe((response)=>{
+            console.log(response);
+        })
+        // alert(JSON.stringify(this.playerform.value))
+        this.clearForm();
+    
+        
+    }
+
+    get controls()
+    {
+        return this.playerform.controls
+    }
+
+    clearForm() {
+
+        this.playerform.reset({
+              'name': '',
+              'age': 18,
+              'id': '',
+              'team': '',
+              'no_of_matches':0,
+              'type':'Allrounder'
+             });
+        }
 }
